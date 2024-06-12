4a3cdfede845dbfea46c1e8be18c82e78287e3d0
Haocheng <haocheng382@gmail.com>
2024-06-12 05:13:22 -0700

    map info

--- src/bridge_pkg/script/message_sender.py
+++ src/bridge_pkg/script/message_sender.py
@@ -6,6 +6,7 @@ import json
 import asyncio
 from std_msgs.msg import String
 from sensor_msgs.msg import Image, CompressedImage
+from nav_msgs.msg import OccupancyGrid
 from visualization_msgs.msg import Marker
 
 URI = 'ws://131.170.250.219:9090'
@@ -32,7 +33,6 @@ def send_robot_direction():
         'msg': robot_description
     }
     asyncio.run(send_data(URI, message))
-
 
 def marker_msgs_callback(data):
     rospy.loginfo("Received Marker message")
@@ -131,19 +131,61 @@ def compressed_image_msgs_callback(data):
     }
     asyncio.run(send_data(URI, message))
 
+def map_msgs_callback(data):
+    rospy.loginfo("Received Map message")
+    message = {
+        'op': 'publish',
+        'topic': '/map',
+        'msg': {
+            'header': {
+                'frame_id': data.header.frame_id,
+                'stamp': {
+                    'secs': data.header.stamp.secs,
+                    'nsecs': data.header.stamp.nsecs
+                }
+            },
+            'info': {
+                'map_load_time': {
+                    'secs': data.info.map_load_time.secs,
+                    'nsecs': data.info.map_load_time.nsecs
+                },
+                'resolution': data.info.resolution,
+                'width': data.info.width,
+                'height': data.info.height,
+                'origin': {
+                    'position': {
+                        'x': data.info.origin.position.x,
+                        'y': data.info.origin.position.y,
+                        'z': data.info.origin.position.z,
+                    },
+                    'orientation': {
+                        'x': data.info.origin.orientation.x,
+                        'y': data.info.origin.orientation.y,
+                        'z': data.info.origin.orientation.z,
+                        'w': data.info.origin.orientation.w,
+                    },
+                },
+            },
+            'data': list(data.data)  # Ensure the data is a list for JSON serialization
+        }
+    }
+    asyncio.run(send_data(URI, message))
+
 def listener():
     rospy.init_node('message_sender', anonymous=True)
 
     asyncio.run(advertise(URI, '/robot_description', 'std_msgs/String'))
     asyncio.run(advertise(URI, '/visualization_marker', 'visualization_msgs/Marker'))
-    asyncio.run(advertise(URI, '/xtion/rgb/image_raw', 'sensor_msgs/Image'))
+    # asyncio.run(advertise(URI, '/xtion/rgb/image_raw', 'sensor_msgs/Image'))
     asyncio.run(advertise(URI, '/xtion/rgb/image_raw/compressed', 'sensor_msgs/CompressedImage'))
+    asyncio.run(advertise(URI, '/map', 'nav_msgs/OccupancyGrid'))
 
     send_robot_direction()
 
     rospy.Subscriber('/visualization_marker', Marker, marker_msgs_callback)
-    rospy.Subscriber('/xtion/rgb/image_raw', Image, image_msgs_callback)
+    # rospy.Subscriber('/xtion/rgb/image_raw', Image, image_msgs_callback)
     rospy.Subscriber('/xtion/rgb/image_raw/compressed', CompressedImage, compressed_image_msgs_callback)
+    rospy.Subscriber('/map', OccupancyGrid, map_msgs_callback)
     rospy.spin()
 
 if __name__ == '__main__':
